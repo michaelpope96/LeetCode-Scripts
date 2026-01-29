5bef498dc20c7d47cb9f5ba8c8692459956dbf4b
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-01-29 14:41:30 -0800

    Time: 0 ms (100%), Space: 7.7 MB (91.6%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int matches = 0;
+        int temp = 0;
+        while (n > 1) {
+            temp = 0;
+            if (n % 2 == 1) {
+                temp = (n - 1) / 2;
+                matches += temp;
+                n = (temp + 1);
+            } else {
+                n /= 2;
+                matches += n;
+            }
+
+            std::cout << "Teams = " << n << " Matches = " << matches << ", and " << temp << " teams advance\n";
+        }
+
+        return matches;
+    }
+};

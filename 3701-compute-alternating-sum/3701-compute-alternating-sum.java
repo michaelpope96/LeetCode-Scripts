b5adc7a88a0829dfb4df0b83545cee279cd9ec91
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-01-29 13:11:43 -0800

    Time: 1 ms (98.01%), Space: 46.4 MB (79.06%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (i % 2 == 0) {
+                result += nums[i];
+            } else {
+                result -= nums[i];
+            }
+        }
+
+        return result;
+    }
+}

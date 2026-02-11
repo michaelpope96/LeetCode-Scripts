d945b6c74d10296b2d4dfbc51b5d8ef07280a864
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-02-10 17:48:22 -0800

    Time: 27 ms (3%), Space: 14.4 MB (5.12%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -11,42 +11,41 @@ public:
     }
 
     int determineMinDistance(vector<int> start, vector<int> end) {
-        vector<int> current = start;
         int time = 0;
-        while(current[0] != end[0] || current[1] != end[1]) {
-            if (end[0] - current[0] > 0) {
-                if (end[1] - current[1] > 0) {
+        while(start[0] != end[0] || start[1] != end[1]) {
+            if (end[0] - start[0] > 0) {
+                if (end[1] - start[1] > 0) {
                     // move diagonally up to the right
-                    current[0] += 1;
-                    current[1] += 1;
-                } else if (end[1] - current[1] < 0) {
+                    start[0] += 1;
+                    start[1] += 1;
+                } else if (end[1] - start[1] < 0) {
                     // move diagonally down to the right
-                    current[0] += 1;
-                    current[1] -= 1;
+                    start[0] += 1;
+                    start[1] -= 1;
                 } else {
                     // move straight right
-                    current[0] +=1;
+                    start[0] +=1;
                 }
-            } else if (end[0] - current[0] < 0) {
-                 if (end[1] - current[1] > 0) {
+            } else if (end[0] - start[0] < 0) {
+                 if (end[1] - start[1] > 0) {
                     // move diagonally up to the left
-                    current[0] -= 1;
-                    current[1] += 1;
-                } else if (end[1] - current[1] < 0) {
+                    start[0] -= 1;
+                    start[1] += 1;
+                } else if (end[1] - start[1] < 0) {
                     // move diagonally down to the left
-                    current[0] -= 1;
-                    current[1] -= 1;
+                    start[0] -= 1;
+                    start[1] -= 1;
                 } else {
                     // move straight left
-                    current[0] -=1;
+                    start[0] -=1;
                 }
             } else {
-                if (end[1] - current[1] > 0) {
+                if (end[1] - start[1] > 0) {
                     // move straight up
-                    current[1] += 1;
-                } else if (end[1] - current[1] < 0) {
+                    start[1] += 1;
+                } else if (end[1] - start[1] < 0) {
                     // move straight down
-                    current[1] -= 1;
+                    start[1] -= 1;
                 } else {
                     // we found the target
                 }

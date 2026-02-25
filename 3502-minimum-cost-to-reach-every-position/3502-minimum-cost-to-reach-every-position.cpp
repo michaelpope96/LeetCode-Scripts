7cccb753cc0f5c87af16c741bce66ae0b97abf8f
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-02-24 17:42:53 -0800

    Time: 0 ms (100%), Space: 26.8 MB (36.7%) - LeetHub

--- 3502-minimum-cost-to-reach-every-position/3502-minimum-cost-to-reach-every-position.cpp
+++ 3502-minimum-cost-to-reach-every-position/3502-minimum-cost-to-reach-every-position.cpp
@@ -2,19 +2,11 @@ class Solution {
 public:
     std::vector<int> minCosts(const std::vector<int>& costs) {
         const unsigned long n = costs.size();
-        std::vector<int> answer;
-        answer.reserve(n);
-
-        int maxPrice = costs[0];
-        answer.emplace_back(maxPrice);
+        std::vector<int> answer(n, -1);
+        answer[0] = costs[0];
 
         for (unsigned long i = 1; i < n; ++i) {
-            if (costs[i] < maxPrice) {
-                answer.emplace_back(costs[i]);
-                maxPrice = costs[i];
-            } else {
-                answer.emplace_back(answer[i - 1]);
-            }
+            answer[i] = std::min(answer[i - 1], costs[i]);
         }
 
         return answer;

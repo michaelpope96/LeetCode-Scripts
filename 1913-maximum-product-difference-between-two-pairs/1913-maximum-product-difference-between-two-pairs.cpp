b09c23317542ff6c085aaa234c3240ca9bde7d88
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-02-25 19:52:19 -0800

    Time: 15 ms (90.14%), Space: 27.8 MB (7.04%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int maxProductDifference(vector<int>& nums) {
-        std::priority_queue<int, std::vector<int>, std::less<int>> maxHeap;
-        std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    int maxProductDifference(const std::vector<int>& nums) {
+        std::priority_queue<int> maxHeap;
+        std::priority_queue<int, std::vector<int>, std::greater<>> minHeap;
         for (const int& num : nums) {
             maxHeap.push(num);
             minHeap.push(num);
         }
-        
-        int max = maxHeap.top();
+
+        const int max = maxHeap.top();
         maxHeap.pop();
 
-        int min = minHeap.top();
+        const int min = minHeap.top();
         minHeap.pop();
         return (max * maxHeap.top()) - (min * minHeap.top());
     }

19062bf988692c4bcf03bbdfd33d19a745c9db1b
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-03-04 17:52:57 -0800

    Time: 3 ms (79.7%), Space: 8.4 MB (92.84%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -4,12 +4,9 @@ public:
         unsigned long n = s.size();
         std::string tmp;
         while (n > 2) {
-            tmp = std::string(n - 1, '0');
-            for (int i = 0; i < (n - 1); ++i) {
-                tmp[i] = (((s[i] - '0') + (s[i + 1] - '0')) % 10) + '0';
+            for (int i = 0; i < n - 1; ++i) {
+                s[i] = (((s[i] - '0') + (s[i + 1] - '0')) % 10) + '0';
             }
-            std::cout << "tmp: " << tmp << std::endl;
-            s = tmp;
             --n;
         }
 

21331c45607398873319752824cc6d46e7bf2848
Michael Pope <36758185+michaelpope96@users.noreply.github.com>
2026-03-05 12:13:56 -0800

    Time: 0 ms (100%), Space: 26.9 MB (5.1%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.cpp
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int maximumNumberOfStringPairs(const std::vector<std::string>& words) {
         int answer = 0;
-        std::set<std::string> wordSet;
+        std::unordered_set<std::string> wordSet;
+        std::string reversed;
 
         for (const std::string& word : words) {
-            wordSet.insert(word);
-        }
+            reversed = std::string(word.rbegin(), word.rend());
 
-        for (const std::string& word : words) {
-            std::string reversed = std::string(word.rbegin(), word.rend());
             if (reversed == word) {
-                wordSet.erase(word);
                 continue;
             } else if (wordSet.find(reversed) != wordSet.end()) {
                 ++answer;
-                wordSet.erase(word);
                 wordSet.erase(reversed);
+            } else {
+                wordSet.insert(word);
             }
         }
 
